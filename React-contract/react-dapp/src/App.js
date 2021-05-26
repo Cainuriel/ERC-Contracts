aa4517d1870b68e1eec7af763a291ae52b56be6c
Cainuriel <cainuriel@gmail.com>
2021-05-26 12:50:16 +0200

    React-contract

---
+++ React-contract/react-dapp/src/App.js
@@ -0,0 +1,60 @@
+import './App.css';
+import { useState } from 'react';
+import { ethers } from 'ethers'
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json'
+
+// Update with the contract address logged out to the CLI when it was deployed 
+const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+
+function App() {
+  // store greeting in local state
+  const [greeting, setGreetingValue] = useState('')
+
+  // request access to the user's MetaMask account
+  async function requestAccount() {
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+  }
+
+  // call the smart contract, read the current greeting value
+  async function fetchGreeting() {
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
+      try {
+        const data = await contract.greet()
+        console.log('data: ', data)
+      } catch (err) {
+        console.log("Error: ", err)
+      }
+    }    
+  }
+
+  // call the smart contract, send an update
+  async function setGreeting() {
+    if (!greeting) return
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount()
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer)
+      const transaction = await contract.setGreeting(greeting)
+      await transaction.wait()
+      fetchGreeting()
+    }
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button onClick={fetchGreeting}>Lee el mensaje</button>
+        <button onClick={setGreeting}>Mandar</button>
+        <input onChange={e => setGreetingValue(e.target.value)} 
+        placeholder="Introduce uno nuevo" 
+        value={greeting}
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
