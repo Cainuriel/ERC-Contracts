343655f3d5c9f83b78d8f3cc93c7acca2d500127
Fernando Lopez <cainuriel@gmail.com>
2023-12-13 21:54:10 +0100

    build in a add image processing.gif in payment-channel

--- payment-channel/hardhat.config.js
+++ payment-channel/hardhat.config.js
@@ -3,7 +3,7 @@ require('@nomiclabs/hardhat-ethers');
 require('dotenv').config();
 
 
-const { PRIVATE_KEY } = process.env;
+const { PRIVATE_KEY_TESTNET } = process.env;
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -33,13 +33,13 @@ task("accounts", "Prints the list of accounts", async () => {
       url: "https://data-seed-prebsc-1-s1.binance.org:8545",
       chainId: 97,
       gasPrice: 20000000000,
-      accounts: [`0x${PRIVATE_KEY}`]
+      accounts: [`0x${PRIVATE_KEY_TESTNET}`]
     },
     mainnet: {
       url: "https://bsc-dataseed.binance.org/",
       chainId: 56,
       gasPrice: 20000000000,
-      accounts: [`0x${PRIVATE_KEY}`]
+      accounts: [`0x${PRIVATE_KEY_TESTNET}`]
     }
   },
   solidity: {
